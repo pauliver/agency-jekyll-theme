e0b2be408090ceb3ca7b7b448a5da6634d95cbf1
Paul Oliver <Pauliver@gmail.com>
2020-04-05 19:16:09 -0700

    next stage of upgrade to jQuery 3.1.0

--- js/contact_me.js
+++ js/contact_me.js
@@ -57,7 +57,7 @@ $(function() {
         },
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").on("click",function(e) {
         e.preventDefault();
         $(this).tab("show");
     });
--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -43,7 +43,7 @@
           }).toArray()
         );
 
-        $(uniqueForms).bind("submit", function (e) {
+        $(uniqueForms).on("submit", function (e) {
           var $form = $(this);
           var warningsFound = 0;
           var $inputs = $form.find("input,textarea,select").not("[type=submit],[type=image]").filter(settings.options.filter);
@@ -398,7 +398,7 @@
           //                                                    VALIDATION
           // =============================================================
 
-          $this.bind(
+          $this.on(
             "validation.validation",
             function (event, params) {
 
@@ -421,7 +421,7 @@
             }
           );
 
-          $this.bind(
+          $this.on(
             "getValidators.validation",
             function () {
               return validators;
@@ -431,13 +431,13 @@
           // =============================================================
           //                                             WATCH FOR CHANGES
           // =============================================================
-          $this.bind(
+          $this.on(
             "submit.validation",
             function () {
               return $this.triggerHandler("change.validation", {submitting: true});
             }
           );
-          $this.bind(
+          $this.on(
             [
               "keyup",
               "focus",
@@ -498,7 +498,7 @@
               }
             }
           );
-          $this.bind("validationLostFocus.validation", function () {
+          $this.on("validationLostFocus.validation", function () {
             $controlGroup.removeClass("success");
           });
         });
@@ -514,7 +514,7 @@
               $helpBlock = $controlGroup.find(".help-block").first();
 
             // remove our events
-            $this.unbind('.validation'); // events are namespaced.
+            $this.off('.validation'); // events are namespaced.
             // reset help text
             $helpBlock.html($helpBlock.data("original-contents"));
             // reset classes
@@ -699,7 +699,7 @@
 				name: "match",
 				init: function ($this, name) {
 					var element = $this.parents("form").first().find("[name=\"" + $this.data("validation" + name + "Match") + "\"]").first();
-					element.bind("validation.validation", function () {
+					element.on("validation.validation", function () {
 						$this.trigger("change.validation", {submitting: true});
 					});
 					return {"element": element};
@@ -754,7 +754,7 @@
 				name: "maxchecked",
 				init: function ($this, name) {
 					var elements = $this.parents("form").first().find("[name=\"" + $this.attr("name") + "\"]");
-					elements.bind("click.validation", function () {
+					elements.on("click.validation", function () {
 						$this.trigger("change.validation", {includeEmpty: true});
 					});
 					return {maxchecked: $this.data("validation" + name + "Maxchecked"), elements: elements};
@@ -769,7 +769,7 @@
 				name: "minchecked",
 				init: function ($this, name) {
 					var elements = $this.parents("form").first().find("[name=\"" + $this.attr("name") + "\"]");
-					elements.bind("click.validation", function () {
+					elements.on("click.validation", function () {
 						$this.trigger("change.validation", {includeEmpty: true});
 					});
 					return {minchecked: $this.data("validation" + name + "Minchecked"), elements: elements};
